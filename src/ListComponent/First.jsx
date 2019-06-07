b246cd8d5136c61192f54a422dcb291127555390
gszasza1 <gszasza1@gmail.com>
2019-06-07 23:36:25 +0200

    init

---
+++ src/ListComponent/First.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './First.css'
+
+class First extends React.Component {
+
+    render() {
+        return (
+            <div className="all_padding">
+              <span className="all_title">Fading</span>  
+            </div>
+        );
+    }
+
+}
+export default First;
---
+++ src/SimpleComponent/Origin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Origin.css'
+
+const Origin = () => (
+    <div className="origin_full">
+        <div className="origin_text_container">
+            <span className="origin_text">React & Css transistion</span>
+            </div>
+        <li className="origin_tips">Tips for Design development</li>
+    </div>
+)
+export default Origin;
